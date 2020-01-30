dfdb4ddf3228666af91ecaa2d6362becb3e6a8e2
Aditi <aditi.dam@gmail.com>
2020-01-30 11:32:16 -0500

    turtle face missing sad aaaaah

--- turtleHangman.py
+++ turtleHangman.py
@@ -41,12 +41,16 @@ t.down()
 
 # Leg 1
 t.fd(100)
+
 t.up()
 t.rt(180)
 t.fd(100)
 t.rt(90)
 t.down()
+
+# Leg 2
 t.fd(100)
+
 t.up()
 t.rt(180)
 t.fd(100)
@@ -56,11 +60,18 @@ t.fd(255)
 t.lt(90)
 t.fd(20)
 t.down()
+
+# Eye 1
 t.circle(5)
+
 t.up()
 t.rt(180)
-t.fd(20)
+t.fd(30)
+t.rt(90)
+t.fd(4)
 t.down()
+
+# Eye 2
 t.circle(5)
 
 
