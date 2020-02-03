0fa3f12821dbfd25ba48f3bf3160f096fdca498e
satwika-vemuri <satwika.vemuri@gmail.com>
2020-02-03 07:47:29 -0500

    Finished half the letter buttons

--- playScreen.py
+++ playScreen.py
@@ -1,6 +1,5 @@
 from tkinter import *
-
-
+import turtle
 class Play_screen(Frame):
 
     def __init__(self, master, choice):
@@ -16,17 +15,28 @@ class Play_screen(Frame):
                     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
         column = 0
+        column2 = 0
 
         for letter in alphabet:
-            self.letter = Button(self, text=letter, fg="white", bg="navy",
-                                 command=self.letter_click
-                                 ).grid(row=20, column=column, sticky=N)
+            if column <= 25:
+                self.letter = Button(self, text=letter, fg="white", bg="navy",
+                                    command=self.letter_click
+                                    ).grid(row=20, column=column, sticky=N)
 
-            Label(self, text=""
-                  ).grid(row=20, column=column + 1, sticky=N)
+                Label(self, text=""
+                    ).grid(row=20, column=column + 1, sticky=N)
+                column += 2
+            else:
+                self.letter = Button(self, text=letter, fg="white", bg="navy",
+                                     command=self.letter_click
+                                     ).grid(row=22, column=column2, sticky=N)
 
-            column += 2
+                Label(self, text=""
+                      ).grid(row=22, column=column2 + 1, sticky=N)
 
+                column2 += 2
+        Label(self, text=""
+              ).grid(row=21, sticky=N)
 
 # Is there now way to make a function called def letters(self): and have it work on each button as specified.
 # Maybe name the buttons like self.a, self.b, etc.?
