2c7cc8c70f38f3c3dab86250fdbd9500be642a64
Aparna <aparnarenjithpillai@gmail.com>
2020-02-03 08:16:30 -0500

    Aparna Version 2/3/20

--- instructionsPage.py
+++ instructionsPage.py
@@ -17,7 +17,7 @@ class Instruction_page(Frame):
 
        instructions = '''Are you ready to play one of the best games in the world?\n\nWith this version of Hangman, you get a chance to
 play the classic game, while watching your hangman being drawn line by line!\n\nCan you save your hangman?\n\n\nFirst, select the single player button. \n\nThen, choose from one of the four categories to get words of that type.\n\nIf you are playing with a
-friend, you can also click the "choose your own word" button to have your friend choose a custom word for you!\nFinally, click the letter buttons to spell out the word, and win before it's too late!'''
+friend, you can also click the "choose your own word" button to have your friend choose a custom word for you!\nFinally, click the letter buttons to spell out the word, and win before it's too late! Keep in mind that you only have 10 tries to get it right.'''
 
        self.story_txt.delete(0.0, END)
        self.story_txt.insert(0.0, instructions)
