4afc7b5d5286beb667f1544388c54ff2045238a8
satwika-vemuri <satwika.vemuri@gmail.com>
2020-02-03 08:32:57 -0500

    2/3/20

--- turtleHangman.py
+++ turtleHangman.py
@@ -9,49 +9,53 @@ class Drawing(Frame):
         self.stand()
         self.head()
         self.body()
-        self.arm1()
-        self.arm2()
-        self.leg1()
-        self.leg2()
-        self.eye1()
-        self.eye2()
-        self.mouth()
+        # self.arm1()
+        # self.arm2()
+        # self.leg1()
+        # self.leg2()
+        # self.eye1()
+        # self.eye2()
+        # self.mouth()
 
 
-        def stand():
-            t = turtle.Turtle()
-            win = turtle.Screen()
-            t.setpos(0, 0)
-            t.speed(0)
-            t.up()
-            t.lt(180)
-            t.fd(180)
-            t.lt(90)
-            t.fd(260)
-            t.rt(90)
-            t.down()
-            t.fd(50)
-            t.rt(180)
-            t.fd(100)
-            t.lt(180)
-            t.fd(50)
-            t.rt(90)
-            t.fd(500)
-            t.rt(90)
-            t.fd(180)
-            t.rt(90)
-            t.fd(50)
-            t.up()
-            t.fd(100)
-            t.lt(90)
-            t.down()
+    def stand(self):
+        t = turtle.Turtle()
+        win = turtle.Screen()
+        t.setpos(0, 0)
+        t.speed(0)
+        t.up()
+        t.lt(180)
+        t.fd(180)
+        t.lt(90)
+        t.fd(260)
+        t.rt(90)
+        t.down()
+        t.fd(50)
+        t.rt(180)
+        t.fd(100)
+        t.lt(180)
+        t.fd(50)
+        t.rt(90)
+        t.fd(500)
+        t.rt(90)
+        t.fd(180)
+        t.rt(90)
+        t.fd(50)
+        t.up()
+        t.fd(100)
+        t.lt(90)
+        t.down()
 
-# head
-t.circle(50)
+    def head(self):
+        t = turtle.Turtle()
+        win = turtle.Screen()
+        t.setpos(0, 0)
+        t.speed(0)
+        t.up()
+        t.circle(50)
+        t.rt(90)
 
-t.rt(90)
-
-    def body():
+    def body(self):
         t = turtle.Turtle()
         win = turtle.Screen()
         t.setpos(0, 0)
@@ -62,74 +66,74 @@ t.rt(90)
         t.rt(180)
         t.up()
 
-# Arm 1
-t.fd(100)
+# # Arm 1
+# t.fd(100)
+#
+# t.rt(45)
+# t.down()
+# t.fd(100)
+# t.up()
+# t.rt(180)
+# t.fd(100)
+# t.rt(90)
+# t.down()
+#
+# # Arm 2
+# t.fd(100)
+#
+# t.up()
+# t.rt(180)
+# t.fd(100)
+# t.rt(45)
+# t.fd(100)
+# t.rt(45)
+# t.down()
+#
+# # Leg 1
+# t.fd(100)
+#
+# t.up()
+# t.rt(180)
+# t.fd(100)
+# t.rt(90)
+# t.down()
+#
+# # Leg 2
+# t.fd(100)
+#
+# t.up()
+# t.rt(180)
+# t.fd(100)
+# t.rt(45)
+# t.up()
+# t.fd(255)
+# t.lt(90)
+# t.fd(20)
+# t.down()
+#
+# # Eye 1
+# t.circle(5)
+#
+# t.up()
+# t.rt(180)
+# t.fd(30)
+# t.rt(90)
+# t.fd(4)
+# t.down()
+#
+# # Eye 2
+# t.circle(5)
+#
+# t.up()
+# t.fd(30)
+# t.rt(180)
+# t.down()
+#
+# # Mouth
+# t.circle(15, 180)
+#
+# t.up()
+# t.fd(100)
+#
 
-t.rt(45)
-t.down()
-t.fd(100)
-t.up()
-t.rt(180)
-t.fd(100)
-t.rt(90)
-t.down()
-
-# Arm 2
-t.fd(100)
-
-t.up()
-t.rt(180)
-t.fd(100)
-t.rt(45)
-t.fd(100)
-t.rt(45)
-t.down()
-
-# Leg 1
-t.fd(100)
-
-t.up()
-t.rt(180)
-t.fd(100)
-t.rt(90)
-t.down()
-
-# Leg 2
-t.fd(100)
-
-t.up()
-t.rt(180)
-t.fd(100)
-t.rt(45)
-t.up()
-t.fd(255)
-t.lt(90)
-t.fd(20)
-t.down()
-
-# Eye 1
-t.circle(5)
-
-t.up()
-t.rt(180)
-t.fd(30)
-t.rt(90)
-t.fd(4)
-t.down()
-
-# Eye 2
-t.circle(5)
-
-t.up()
-t.fd(30)
-t.rt(180)
-t.down()
-
-# Mouth
-t.circle(15, 180)
-
-t.up()
-t.fd(100)
-
-win.exitonclick()
-
+#
