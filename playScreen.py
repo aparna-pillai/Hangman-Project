4337fadabb0f4e56e9830f67a132f7575989a492
Aditi <aditi.dam@gmail.com>
2020-02-03 11:16:40 -0500

    aditi does things

--- playScreen.py
+++ playScreen.py
@@ -12,21 +12,35 @@ class Play_screen(Frame):
         self.create_widgets()
 
     def create_widgets(self):
+
         guess_list = []
 
         for letter in self.choice:
             if letter!="\n":
                 guess_list.append(letter)
+        let_str =""
+        for item in guess_list:
+            if item == "_":
+                let_str +=" "
+
+            else:
+                let_str += "_ "
+
+        Label(self,
+              text=let_str, font="Courier 20 bold",
+              fg="Navy Blue").grid(row=1, column=1, columnspan = 1000, sticky=N)
+        Label(self,
+              text="", font="Courier 20 bold").grid(row=2, column=1, sticky=N)
 
         Drawing.stand(self)
         alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-        column = 0
-        column2 = 0
+        column = 1
+        column2 = 1
 
         for letter in alphabet:
-            if column <= 25:
+            if column <= 26:
                 self.letter = Button(self, text=letter, fg="white", bg="navy",
                                     command=self.create_letter_click_command(letter,guess_list)
                                     ).grid(row=20, column=column, sticky=N)
@@ -54,8 +68,16 @@ class Play_screen(Frame):
     def letter_click(self,letter,list):
         self.guess_list = list
         for item in self.guess_list:
+            letter_count = 0
             if item == letter:
-                pass
+                let_str = ""
+                for item in self.guess_list:
+                    pass
+                Label(self,
+                      text=let_str, font="Courier 20 bold",
+                      fg="Navy Blue").grid(row=1, column=1, columnspan=1000, sticky=N)
+
+            letter_count += 1
 
 
 
