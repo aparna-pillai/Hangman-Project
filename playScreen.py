97f36c9be191ec2ab893e49acfab07d865f5f0e3
satwika-vemuri <satwika.vemuri@gmail.com>
2020-02-04 08:57:16 -0500

    2/3/20

--- playScreen.py
+++ playScreen.py
@@ -67,12 +67,29 @@ class Play_screen(Frame):
     def letter_click(self,letter,list, string):
         self.guess_list = list #all the letters in the player's choice
         self.let_str = string
+        self.totalcount = 1
+        self.bodypartcount = 0
         for item in self.guess_list:
             if letter != item:
                 self.let_str += "_ "
             else:
+                self.totalcount = 0
                 self.let_str += letter
+        if self.totalcount == 1:
+            # GO TO TURTLEHANGMAN.PY AND FINISH WRITING THE FUNCTIONS. WHAT DEF LETTER_CLICK DOES IS IT TAKES THE LETTER
+            # THAT THE USER CHOSE AND COMPARES EVERY ITEM IN THE GUESS_LIST (GUESS_LIST CONTAINS ALL THE LETTERS OF THE WORD THEY HAVE TO GUESS)
+            # TO THE LETTER THAT THEY CHOSE. IF THE LETTER ISN'T EQUAL TO ONE OF THE LETTERS IN THE LIST, THEN IT TAKES A STRING AND ADDS A BLANK TO IT ("_")
+            # IF THE LETTER IS EQUAL TO TO ONE OF THE LETTERS IN THE LIST, IT ADDS THAT LETTER TO THE STRING. IT ALSO MAKES SELF.TOTALCOUNT = 0
+            # NOTE THAT SELF.TOTALCOUNT EQUALS ONE AT FIRST. IF AT THE END OF THE ENTIRE FOR LOOP, SELF.TOTALCOUNT ISN'T EQUAL TO 0, THAT MEANS THAT
+            # THE USER'S GUESS IS WRONG. IF IT IS, THEN IT GOES TO THE STATEMENT "IF SELF.TOTALCOUNT == 1:"
+            # IN THIS STATEMENT, WE HAVE TO CALL ONE OF THE FUNCTIONS IN TURTLEHANGMAN.PY SO THAT A BODY PART IS DRAWN.
+            # MAKE SURE THAT YOU GUYS WRITE IT SO THAT EACH TIME THE USER MESSES UP A GUESS, A different BODY PART IS ADDED.
+
+
+        # APARNA/ADITI: ASK MR.WANG WHY THE BELOW LINE DOESN'T REPLACE THE TEXT IN THE LABEL
         self.guess_label['text'] = self.let_str
+        # AFTER YOU ARE DONE WITH THAT, YOU GUYS CAN CONTINUE WORKING ON THE CODE. WE STILL HAVE THE PROBLEM THAT I SHOWED YOU GUYS THIS MORNING!!
+
 
         #Label(self,
               #text=self.let_str, font="Courier 20 bold", fg="Sea Green").grid(row=1, column=1, columnspan=1000,
